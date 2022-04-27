041f913d53afc1a0b7534749c3b697d98cbc31d3
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2022-04-27 01:43:50 -0500

    Se agregan test para los filtros

---
+++ test/services/partnerservice.test.js
@@ -0,0 +1,88 @@
+const PartnerService = require("../../lib/services/PartnerService");
+
+
+describe('Test PartnerService', () => {
+    test('filter Partners with certification ', () => {
+        const partners = [{
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "email": "Todd@visualpartnership.xyz",
+            "credits": 508,
+            "enrollments": [
+              "Visual Thinking Intermedio",
+              "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 1,
+            "haveCertification": true
+          },
+          {
+            "id": "6264d5d85cf81c496446b67f",
+            "name": "Lucinda",
+            "email": "Sexton@visualpartnership.xyz",
+            "credits": 677,
+            "enrollments": [
+              "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 6,
+            "haveCertification": false
+          },
+          {
+            "id": "6264d5d8cda17de0d2e9f236",
+            "name": "Fuentes",
+            "email": "Sharlene@visualpartnership.xyz",
+            "credits": 210,
+            "enrollments": [
+              "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 10,
+            "haveCertification": true
+          }];
+
+        const resp = PartnerService.filterPartnersEmailWithCertification(partners);
+
+        expect(resp.length).toBe(2);
+        // expect(resp[0].haveCertification).toBeTruthy();
+        // expect(resp[1].haveCertification).toBeTruthy();
+    });
+
+    test('filter Partners with credits greater than 500', () => {
+        const partners = [{
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "email": "Todd@visualpartnership.xyz",
+            "credits": 508,
+            "enrollments": [
+              "Visual Thinking Intermedio",
+              "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 1,
+            "haveCertification": true
+          },
+          {
+            "id": "6264d5d85cf81c496446b67f",
+            "name": "Lucinda",
+            "email": "Sexton@visualpartnership.xyz",
+            "credits": 677,
+            "enrollments": [
+              "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 6,
+            "haveCertification": false
+          },
+          {
+            "id": "6264d5d8cda17de0d2e9f236",
+            "name": "Fuentes",
+            "email": "Sharlene@visualpartnership.xyz",
+            "credits": 210,
+            "enrollments": [
+              "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 10,
+            "haveCertification": true
+          }];
+
+        const resp = PartnerService.filterPartnersCredits(partners, 500);
+
+        expect(resp.length).toBe(2);
+    });
+});
